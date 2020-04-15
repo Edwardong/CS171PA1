6c87c50b42628ced0165f5b7651542020458db5b
Edwardong <dongzheren@gmail.com>
2020-04-15 03:49:44 -0700

    想了好久终于想明白了，我真是 jb 弱智

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 from typing import Dict, Any
+from collections import OrderedDict
 
 class Client:
-    event_queue: Dict[int, str]
+    event_queue: OrderedDict[int, str]
 
-    def __init__(self,id):
-        self.local_clock = 1
-        self.event_queue = {}
+    def __init__(self, id):
+        self.local_clock = 0
+        self.event_queue = OrderedDict()
         self.client_id = id
+        self.started = False
 
     def get_clock(self):
         return self.local_clock
@@ -14,19 +16,20 @@ class Client:
     def get_events(self):
         return self.event_queue
 
-    def update_events(self,event):
+    def update_events(self, event):
         self.event_queue[self.local_clock] = event
+        self.started = True
 
-    def update_clock(self, input = 1):
-        self.local_clock = max(self.local_clock, input) + 1
+    def update_clock(self, input_counter=1):
+        self.local_clock = max(self.local_clock, input_counter) + 1
 
     def print_clock(self):
-        all_event = [str(i) for i in [*self.event_queue].sort()]
-        print(" ".join(all_event))
+        if not self.started:
+            print("0")
+        else:
+            #all_event = [str(i) for i in [*self.event_queue].sort()]
+            print("P{} clock: {}.".format(self.client_id,self.event_queue)
         return
 
     def get_pid(self):
         return self.client_id
-
-
-
--- process.py
+++ process.py
@@ -1,23 +1,98 @@
 import re
+import argparse
+import client
+import threading
+import queue
+import socket
+import time
+
+P1PORT = 5001
+P2PORT = 5002
+P3PORT = 5003
+
+shared_queue = queue.Queue()
 
 """ input format:
     local event_name
     send P_i event_name """
-def process_str(input):
-    rule = re.compile(r"[^a-zA-Z0-9]")
-    if input[:5] == "local":
-        input_list = input.strip().split()
-        for i in range(len(input_list)):
-            input_list[i] = rule.sub('',input_list[i])
-        return (input_list[0], input_list[1])
-    else:
-        receiver = input[5:7]
-        message = input[8:]
-        return ("send", receiver, message)
+# def process_str(input):
+#     rule = re.compile(r"[^a-zA-Z0-9]")
+#     message = ''
+#     if input[:5] == "local":
+#         input_list = input.strip().split()
+#         event = input[6:]
+#         for i in range(len(input_list)):
+#             input_list[i] = rule.sub('',input_list[i])
+#         return ("local", event, message)
+#     else:
+#         receiver = input[5:7]
+#         message = input[8:]
+#         return ("send", receiver, message)
+
+#need to think about this more
+def start_process():
+    #where = ""
+    while True:
+        while shared_queue.empty():
+            pass
+        one_event = shared_queue.get()
+        if one_event[:5] == "local":  # e.g. local Wakeup
+            where = "local"
+            event = input[6:]
+            this_client.update_clock(1)
+            this_client.update_event("local: " + event)
+        elif one_event[:5] == "send":
+            where = "remote"
+            receiver = input[5:7]
+            message = input[8:]
+            this_client.update_clock(1)
+            this_client.update("send: " + receiver + message)
+    return
+
+def send_msg(host,port,local_clock,msg):
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    time.sleep(5)
+    # communication process needs these
+    # if receiver == "P1" or receiver == "p1":
+    #     port = P1PORT
+    # elif receiver == "P2" or receiver == "p2":
+    #     port = P2PORT
+    # elif receiver == "P3" or receiver == "p3":
+    #     port = P3PORT
+    # else:
+    #     print("error receiver id")
+    #     return
+    s.connect((host,port))
+
+    s.send()
+
+
+
+
+
 
 if __name__ == '__main__':
-    # for testing
-    # arg1 = "local Wakeup"
-    # arg2 = "send p3 How are you doing?"
-    # new_text = process_str(arg1)
-    # print(new_text)
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('port',type=int)
+    parser.add_argument('pid', type=int)
+    arg = parser.parse_args()
+    #port = arg.port
+    this_pid = arg.pid
+
+    this_client = client(this_pid)
+    process_thread = threading.Thread(target=start_process, args=this_client)
+
+    while True:
+        one_event = input()
+        if one_event[:5] == "print":
+            this_client.print_clock()
+        else:
+            shared_queue.put(one_event)
+
+
+
+
+
+
+
+
